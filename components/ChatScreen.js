49e42ac15f03b6e5878ecc2c882ab52bc30f12b5
Shashank <shashankgangwar12@gmail.com>
2023-06-25 12:30:33 +0530

    fix backend url

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -6,7 +6,7 @@ import ChatInput from './ChatInput';
 import { View } from 'react-native';
 
 
-const socket = io('http://192.168.1.12:5000'); // Replace with your backend server URL
+const socket = io('https://chatapp-r5ts.onrender.com/'); // Replace with your backend server URL
 const userId = Math.random().toString(16).slice(2);
 
 export default function ChatScreen() {
